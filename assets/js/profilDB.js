4b2c785c9963385ddefdb2f0bbfbcc6de34b1b2b
Wahyu Syafiuddin <wahyu403.py@gmail.com>
2022-01-02 16:44:46 +0700

    :bulb: add Profile Bio

--- assets/js/profilDB.js
+++ assets/js/profilDB.js
@@ -1,8 +1,22 @@
 var infoProfil = document.getElementById('infoProfil')
+var bio = document.getElementById('bioNama')
+
 infoProfil.addEventListener('show.bs.modal', function (event) {
   var button = event.relatedTarget
   var recipient = button.getAttribute('dataNama')
   var modalTitle = infoProfil.querySelector('.modal-title')
 
   modalTitle.textContent = recipient
+  if(recipient == "Muhammad Wahyu Syafi'uddin"){
+    bio.textContent = "ODIN"
+  }
+  else if(recipient == "ANJAY HEKER"){
+      bio.textContent = "Heker Gaool"
+  }
+  else if(recipient == "Mak Lo Heker"){
+      bio.textContent = "Heker Insaf"
+  }else{
+      bio.textContent = "ADA ERROR, TANYA DEVNYA"
+  }
+  
 })
