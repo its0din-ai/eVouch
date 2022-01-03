1c876888cea79ac67493716ca2de6c860e348623
Wahyu Syafiuddin <wahyu403.py@gmail.com>
2022-01-03 09:22:17 +0700

    :bulb: Finish row 1 dan menambahkan Dynamic Img

--- assets/js/productDB.js
+++ assets/js/productDB.js
@@ -1,11 +1,42 @@
-var infoProfil = document.getElementById('productModal')
-infoProfil.addEventListener('show.bs.modal', function (event) {
+// Back to top
+var btn = $("#topp")
+
+btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '100');
+  });
+
+var gaada = document.getElementById("fitur");
+gaada.style.display = "none";
+function kasihTau(){
+    gaada.style.display = "block";
+    console.log("OK")
+}
+
+// DB GAMBAR PRODUK
+var stm = document.getElementById('steam-w')
+var ply = document.getElementById('playstore')
+var rzr = document.getElementById('razer')
+var psg = document.getElementById('playstation')
+
+
+stm.src = "https://images.tokopedia.net/img/attachment/2019/9/27/66301108/66301108_7b8b9fe1-e484-41cf-bd64-ccd417a51b3a.png?ect=4g"
+ply.src = "https://images.tokopedia.net/img/attachment/2020/3/1/66301108/66301108_b0f2723c-bd0f-46ac-ab9c-4d0af068eb73.png?ect=4g"
+rzr.src = "https://images.tokopedia.net/img/attachment/2019/10/7/66301108/66301108_ed814435-e3be-4825-9f4c-37a5609b70ad.png?ect=4g"
+psg.src = "https://images.tokopedia.net/img/attachment/2021/10/27/66301108/66301108_9ecc0f28-2ceb-404f-9a92-da3b3cccbbfb.png?ect=4g"
+  
+  
+
+// Product Info Modal
+var infoProd = document.getElementById('productModal')
+infoProd.addEventListener('show.bs.modal', function (event) {
+    gaada.style.display = "none";
 
 
     var button = event.relatedTarget
-    var recipient = button.getAttribute('name')
-    var modalTitle = infoProfil.querySelector('.modal-title')
-    var coba = infoProfil.querySelector('#coba')
+    var proName = button.getAttribute('name')
+    var modalTitle = infoProd.querySelector('.modal-title')
+    var coba = infoProd.querySelector('#coba')
     var harga = document.getElementById("listVoucher").value
 
     $("#listVoucher").change(function () {
@@ -15,12 +46,29 @@ infoProfil.addEventListener('show.bs.modal', function (event) {
        });
 
 
-    if(recipient == "Steam"){
+    var prodImg = document.getElementById('prodImg')
+    if(proName == "Steam"){
+        prodImg.src= stm.src
         modalTitle.textContent = "Steam Wallet"
-        // coba.textContent = harga
+        coba.textContent = "Pilih Vouchernya"
+
     }
-    else if(recipient == "Google Play"){
+    else if(proName == "Google Play"){
+        prodImg.src= ply.src
         modalTitle.textContent = "Google Play Wallet"
-        // coba.textContent = harga
+        coba.textContent = "Pilih Vouchernya"
+        
+    }
+    else if(proName == "Razer Gold"){
+        prodImg.src= rzr.src
+        modalTitle.textContent = "Razer Gold"
+        coba.textContent = "Pilih Vouchernya"
+        
+    }
+    else if(proName == "PS Gift Card"){
+        prodImg.src= psg.src
+        modalTitle.textContent = "Playstation Gift Card"
+        coba.textContent = "Pilih Vouchernya"
+        
     }
 })
